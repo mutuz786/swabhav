4466ce7007d22fe98c7618fabbfb1025ee070400
mutuz786 <khilawala62@gmail.com>
2020-02-13 15:56:37 +0530

    13 feb 1.1

--- OOP Assignments/assignment3/src/com/techlab/classreflectiontest/ClassReflectionTest.java
+++ OOP Assignments/assignment3/src/com/techlab/classreflectiontest/ClassReflectionTest.java
@@ -1,88 +1,49 @@
 package com.techlab.classreflectiontest;
 
-import java.io.File;
 import java.util.Scanner;
-//import java.lang.reflect.*;
+import java.lang.reflect.*;
 //import java.lang.*;
 
 public class ClassReflectionTest {
+	private static Scanner scan;
 	public static void main(String [] args) throws ClassNotFoundException {
 		System.out.println("Enter the address:");
-		Scanner scan=new Scanner(System.in);
-		String classDirectory=scan.nextLine();
-		classDirectory=createDirectory(classDirectory);
-		//Class<? extends Class> start=(Class<? extends Class>) Class.forName("ClassReflectionTest");
-		//ClassLoader loader=start.getClassLoader();
-		//Class<? extends Class> object=(Class<? extends Class>) Class.forName(classDirectory, false, loader);
-		//Class<? extends Class> test=object.getClass();
-		//System.out.println("Class Name:"+ test.getName());
+		String classDirectory=getDirectory();
+		try   {  
+			Class<?> cls = Class.forName(classDirectory);  
+			System.out.println("Class Name: " + cls.getName());  
+			System.out.println("Package Name: " + cls.getPackage());  
+			Method[] methods = cls.getDeclaredMethods();  
+			System.out.println("-----Methods of String class -------------");  
+			for (Method method : methods)   
+			{  
+				System.out.println(method.getName());  
+			}  
+		}  
+		catch (ClassNotFoundException e)   {  
+			e.printStackTrace();  
+		}
 	}
 
-		public static String createDirectory(String classDirectory) {
-			String[] splitDirectory=new String[3];
-			splitDirectory=classDirectory.split(".");
-			String buffer=null;
-			for(int i=0;i<2;i++) {
-				splitDirectory[i]=splitDirectory[i].toLowerCase();
-				buffer=buffer.concat(splitDirectory[i]);
-				System.out.println(buffer);
-			}
-			classDirectory=buffer.concat(splitDirectory[splitDirectory.length-1]);
-			//classDirectory=classDirectory.replace('.', '\\');
-			//classDirectory="C:\\Program Files\\Java\\jdk1.8.0_181\\src\\".concat(classDirectory);
-			//classDirectory=classDirectory.concat(".class");
-			System.out.println(classDirectory);
-			return classDirectory;
+	private static String getDirectory() {
+		scan = new Scanner(System.in);
+		String classDirectory=scan.nextLine();
+		//return getValidDirectory(classDirectory);
+		return classDirectory;
+	}
+
+	private static String getValidDirectory(String classDirectory) {
+		String[] splitDirectory;
+		splitDirectory=classDirectory.split(".");
+		String buffer=null;
+		for(int i=0;i<2;i++) {
+			splitDirectory[i]=splitDirectory[i].toLowerCase();
+			buffer=buffer.concat(splitDirectory[i]);
+			System.out.println(buffer);
 		}
+		classDirectory=buffer.concat(splitDirectory[splitDirectory.length-1]);
+		System.out.println(classDirectory);
+		return classDirectory;
+	}
 }
 //           java.lang.String
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import java.lang.reflect.Method;  
-import java.util.Scanner;
-public class Demo
-{  
-public static void main(String[] args)  
-{  
-System.out.println("Enter The Directory:");
-Scanner scan = new Scanner(System.in);
-String directory=scan.nextLine();
-try   
-{  
-Class<?> cls = Class.forName(directory);  
-System.out.println("Class Name: " + cls.getName());  
-System.out.println("Package Name: " + cls.getPackage());  
-Method[] methods = cls.getDeclaredMethods();  
-System.out.println("-----Methods of String class -------------");  
-for (Method method : methods)   
-{  
-System.out.println(method.getName());  
-}  
-}  
-catch (ClassNotFoundException e)   
-{  
-e.printStackTrace();  
-}  
-}  
-} 
