d2a88f98e6ad9bb96ba5bc419988313b62df11ea
mutuz786 <khilawala62@gmail.com>
2020-03-14 19:15:07 +0530

    4 march

--- Practice/practice/src/com/techlab/practice/Arithematic.java
+++ Practice/practice/src/com/techlab/practice/Arithematic.java
@@ -1,49 +1,49 @@
 package com.techlab.practice;
 
 public class Arithematic {
-	public static double calculateAns(String que) {
-		double opnd1 = findOpnd(que, 1);
-		double opnd2 = findOpnd(que, -1);
-		char optr = findOptr(que);
-		switch (optr) {
+	public static double calculateAns(String problem) {
+		double operand1 = findOperand(problem, 1);
+		double operand2 = findOperand(problem, -1);
+		char operator = findOperator(problem);
+		switch (operator) {
 		case '+':
-			return opnd1 + opnd2;
+			return operand1 + operand2;
 		case '-':
-			return opnd1 - opnd2;
+			return operand1 - operand2;
 		case '/':
-			return opnd1 / opnd2;
+			return operand1 / operand2;
 		case '*':
-			return opnd1 * opnd2;
+			return operand1 * operand2;
 		case '%':
-			return opnd1 % opnd2;
+			return operand1 % operand2;
 		}
 		return 0;
 	}
 
-	private static double findOpnd(String que, int i) {
-		if(i==1)
-			return Double.parseDouble(que.substring(0,findIndex(que)));
-		if(i==-1)
-			return Double.parseDouble(que.substring(findIndex(que)-i,que.length()));
-		return 0;	
+	private static double findOperand(String problem, int choice) {
+		if (choice == 1)
+			return Double.parseDouble(problem.substring(0, findIndex(problem)));
+		if (choice == -1)
+			return Double.parseDouble(problem.substring(findIndex(problem) - choice, problem.length()));
+		return 0;
 	}
 
-	private static char findOptr(String que) {
-		return que.charAt(findIndex(que));
+	private static char findOperator(String problem) {
+		return problem.charAt(findIndex(problem));
 	}
 
-	private static int findIndex(String que) {
-		for (int i = 0; i < que.length(); i++) {
-			if (opPresent(que.charAt(i)))
+	private static int findIndex(String problem) {
+		for (int i = 0; i < problem.length(); i++) {
+			if (isOpPresent(problem.charAt(i)))
 				return i;
 		}
 		return 0;
 	}
 
-	private static boolean opPresent(char quechar) {
-		char[] ops= {'+','-','/','*','%'};
-		for(char op:ops) {
-			if(op==quechar)
+	public static boolean isOpPresent(char character) {
+		char[] ops = { '+', '-', '/', '*', '%' };
+		for (char op : ops) {
+			if (op == character)
 				return true;
 		}
 		return false;
--- Practice/practice/src/com/techlab/practice/Test.java
+++ Practice/practice/src/com/techlab/practice/Test.java
@@ -1,14 +1,81 @@
 package com.techlab.practice;
 
+import java.util.ArrayList;
+
 public class Test {
+	static ArrayList<Double> operands = new ArrayList<Double>();
+	static ArrayList<Character> operators = new ArrayList<Character>();
+
 	public static void main(String[] args) {
-		String que = "9/8";
-		double ans = calculateAns(que);
-		System.out.println("Question:\n" + que + "\nAnswer:\n" + ans);
+		print("92/52+2213-732/53+21");
 	}
 
-	private static double calculateAns(String que) {
-		
-		return 0;
+	private static void print(String problem) {
+		double ans = calculateAnswer(problem);
+		System.out.println("Question:" + problem + "\nAnswer:" + ans);
+	}
+
+	private static double calculateAnswer(String problem) {
+		getData(problem);
+		performOperation();
+		return operands.get(0);
+	}
+
+	private static void performOperation() {
+		for(int i=0;i<operators.size();i++) {
+			if(operators.get(i)=='/') {
+				double quotient=operands.get(i)/operands.get(i+1);
+				updateArray(quotient,i);
+				
+			}
+		}
+		for(int i=0;i<operators.size();i++) {
+			if(operators.get(i)=='*') {
+				double quotient=operands.get(i)*operands.get(i+1);
+				updateArray(quotient,i);
+				
+			}
+		}
+		for(int i=0;i<operators.size();i++) {
+			if(operators.get(i)=='+') {
+				double quotient=operands.get(i)+operands.get(i+1);
+				updateArray(quotient,i);
+				
+			}
+		}
+		for(int i=0;i<operators.size();i++) {
+			if(operators.get(i)=='-') {
+				double quotient=operands.get(i)-operands.get(i+1);
+				updateArray(quotient,i);
+				
+			}
+		}
+	}
+
+	private static void updateArray(double quotient, int index) {
+		//System.out.println(operands.get(i)+" "+operators.get(i)+" "+operands.get(i+1));
+		for(int i=0;i<operators.size();i++) {
+			System.out.print(operands.get(i)+" ");
+			System.out.print(operators.get(i)+" ");
+		}
+		System.out.println(operands.get(operands.size()-1));
+		operands.remove(index);
+		operands.remove(index);
+		operators.remove(index);
+		operands.add(index, quotient);	
+	}
+
+	private static void getData(String problem) {
+		int start = 0;
+		for (int i = 0; i < problem.length(); i++) {
+			if (Arithematic.isOpPresent(problem.charAt(i))) {
+				operands.add(Double.parseDouble(problem.substring(start, i)));
+				operators.add(problem.charAt(i));
+				start = i + 1;
+			}
+			if (i == problem.length() - 1) {
+				operands.add(Double.parseDouble(problem.substring(start, problem.length() - 1)));
+			}
+		}
 	}
 }
