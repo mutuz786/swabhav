6a66600120df5e1a5b9427d62f459cba80435a18
mutuz786 <khilawala62@gmail.com>
2020-03-28 13:28:49 +0530

    28 march

---
+++ OOP/student-app/src/com/techlabs/model/test/StudentCase2.java
@@ -0,0 +1,18 @@
+package com.techlabs.model.test;
+
+import com.techlabs.model.Student;
+
+public class StudentCase2 {
+	public static void main(String[] args) {
+		Student s1 = new Student(101, "abc");
+		printInfo(s1);
+		Student s2 = new Student(102, null);
+		printInfo(s2);
+		Student s3 = new Student(103, "lmno", 8.0f);
+		printInfo(s3);
+	}
+
+	private static void printInfo(Student student) {
+		System.out.println( student.getId() + "\t" + student.getName() + "\t" + student.getCgpa() + "\t" + Student.getCount());
+	}
+}
