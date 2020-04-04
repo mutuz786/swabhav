7ec477dc26bf5d97e5a67a392a9538ab8a7dba00
mutuz786 <khilawala62@gmail.com>
2020-04-04 16:21:00 +0530

    4 april

--- OOP/numberguesser-app/src/com/techlabs/model/Input.java
+++ OOP/numberguesser-app/src/com/techlabs/model/Input.java
@@ -4,13 +4,7 @@ import java.util.Scanner;
 
 public class Input {
 	public int takeInput() {
-		try {
-			Scanner scan = new Scanner(System.in);
-			return scan.nextInt();
-		}
-		catch(Exception e) {
-			System.out.println(e.getStackTrace());
-		}
-		return 0;
+		Scanner scan = new Scanner(System.in);
+		return scan.nextInt();
 	}
 }
