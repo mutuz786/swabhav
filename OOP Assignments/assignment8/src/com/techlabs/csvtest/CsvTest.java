1ab04e4296be489ef720fb6d3daca68d47b391ca
mutuz786 <khilawala62@gmail.com>
2020-04-07 20:27:59 +0530

    7 april

---
+++ OOP Assignments/assignment8/src/com/techlabs/csvtest/CsvTest.java
@@ -0,0 +1,62 @@
+package com.techlabs.csvtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CsvTest {
+	public static void main(String[] args) throws IOException {
+		Product[] products=new Product[10];
+		products = getProduct();
+		for(Product product:products) {
+			if(product!=null)
+				printInfo(product);	
+		}
+		
+	}
+
+	private static Product[] getProduct() throws IOException {
+		Product[] products = new Product[10];
+		String fileName = "products.csv";
+		int index=0;
+		Path path = Paths.get(fileName);
+		BufferedReader buffer = Files.newBufferedReader(path);
+		String line = buffer.readLine();
+		for (int i = 0; i < 10; i++) {
+			line = buffer.readLine();
+			if(line!=null) {
+				String[] attributes = line.split(",");
+				if(attributes.length>2) {
+					attributes=cleanInput(attributes);
+					products[index]=addProduct(attributes);
+					index++;
+				}	
+			}
+			
+		}
+
+		return products;
+	}
+
+	private static Product addProduct(String[] attributes) {
+		int id=Integer.parseInt(attributes[0]);
+		String item=attributes[1];
+		int price=Integer.parseInt(attributes[2]);
+		double discount=Double.parseDouble(attributes[3]);
+		Product product=new Product(id, item, price, discount);
+		return product;
+	}
+
+	private static String[] cleanInput(String[] product) {
+		for(int i=0;i<product.length;i++) {
+			product[i]=product[i].trim();
+		}
+		return product;
+	}
+
+	private static void printInfo(Product product) {
+		System.out.println(product.getId()+" "+product.getItem()+" "+product.getPrice()+" "+product.getDiscount());
+	}
+}
---
+++ OOP Assignments/assignment8/src/com/techlabs/csvtest/Product.java
@@ -0,0 +1,35 @@
+package com.techlabs.csvtest;
+
+public class Product {
+	int id;
+	String item;
+	int price;
+	double discount;
+	
+	public Product(int id,String item,int price,double discount) {
+		this.id=id;
+		this.item=item;
+		this.price=price;
+		this.discount=discount;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getItem() {
+		return item;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public double getDiscount() {
+		return discount;
+	}
+	
+	public double calculatePrice() {
+		return price*discount;
+	}
+}
